e71bcb10b68d914bdf0803db45f6daa18219d9c5
BilalHassen <bilalhassen097@gmail.com>
2024-01-12 13:14:38 -0500

    add basic structure and styling to todo-container

---
+++ src/components/TodoContainer/TodoContainer.js
@@ -0,0 +1,15 @@
+import "../TodoContainer/TodoContainer.scss";
+
+function TodoContainer() {
+  return (
+    <>
+      <section className="todo-container">
+        <h1 className="todo-container__header">
+          Add something you like to get done!
+        </h1>
+      </section>
+    </>
+  );
+}
+
+export default TodoContainer;
