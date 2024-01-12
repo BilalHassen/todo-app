05f9bf47ffe6f70a10f97b5185b1031a63d2a144
BilalHassen <bilalhassen097@gmail.com>
2024-01-12 18:35:07 -0500

    add function for adding todo with a certain structure

--- src/components/TodoContainer/TodoContainer.js
+++ src/components/TodoContainer/TodoContainer.js
@@ -1,15 +1,33 @@
 import "../TodoContainer/TodoContainer.scss";
 import TodoForm from "../TodoForm/TodoForm";
-import React, { useState } from "react";
 import Todo from "../Todo/Todo";
 import EditTodoForm from "../EditTodoForm/EditTodoForm";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 function TodoContainer() {
+  const [todos, setTodo] = useState([]);
+
+  // function to add a new to do to the array of todos
+  const addTodo = (todo) => {
+    setTodo((prevTodos) => {
+      const newTodo = [
+        ...todos,
+        { id: uuidv4(), task: todo, isEdited: false, completed: false },
+      ];
+      console.log(newTodo);
+      return newTodo;
+    });
+  };
+
   return (
     <>
       <section className="todo-container">
         <h1 className="todo-container__header">Add something to get done !</h1>
-        <TodoForm />
-        <Todo />
+        <TodoForm addTodo={addTodo} />
+        <button onClick={() => addTodo("clean the house")}>
+          Add Todo Manually
+        </button>
       </section>
     </>
   );
