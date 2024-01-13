986b947b70fe96eb0708ec264ca41f27d34c4018
BilalHassen <bilalhassen097@gmail.com>
2024-01-13 18:29:12 -0500

    add the deleteTodo function to the todo component to enable delete to do functionality

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -4,18 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { useEffect, useState } from "react";
-function Todo({ task, editTodo }) {
+function Todo({ task, editTodo, deleteTodo }) {
   return (
     <>
       <div className="todo">
-        <p className="todo__text">{task.task}</p>
+        <p>{task.task}</p>
         <div className="todo__icon-container">
           <FontAwesomeIcon
             icon={faPenToSquare}
             className="todo__edit-icon"
             onClick={() => editTodo(task.id)}
           />
-          <FontAwesomeIcon icon={faTrashCan} className="todo__delete-icon" />
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            className="todo__delete-icon"
+            onClick={() => deleteTodo(task.id)}
+          />
         </div>
       </div>
     </>
