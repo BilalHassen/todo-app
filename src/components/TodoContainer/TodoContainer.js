fc7c44f0f594f447802ee90c1b643f6f2e442eb5
BilalHassen <bilalhassen097@gmail.com>
2024-01-13 12:23:12 -0500

    store newly added todos in local storage

--- src/components/TodoContainer/TodoContainer.js
+++ src/components/TodoContainer/TodoContainer.js
@@ -2,11 +2,22 @@ import "../TodoContainer/TodoContainer.scss";
 import TodoForm from "../TodoForm/TodoForm";
 import Todo from "../Todo/Todo";
 import EditTodoForm from "../EditTodoForm/EditTodoForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function TodoContainer() {
   const [todos, setTodo] = useState([]);
+
+  useEffect(() => {
+    // get the todos from local storage and convert the json formatted string
+    // into a javascript object with JSON.parse()
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+    // update the todos state with the todos from the local storage
+    setTodo(storedTodos);
+
+    console.log(todos);
+  }, []);
 
   // function to add a new to do to the array of todos
   const addTodo = (todo) => {
@@ -16,6 +27,8 @@ function TodoContainer() {
         { id: uuidv4(), task: todo, isEdited: false, completed: false },
       ];
       console.log(newTodo);
+      const jsonTodo = JSON.stringify(newTodo);
+      localStorage.setItem("todos", jsonTodo);
       return newTodo;
     });
   };
